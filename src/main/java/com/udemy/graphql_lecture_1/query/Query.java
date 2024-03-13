4fa9371bb9e9e3d5950209be51c32a9f037c6af7
JisooPyo <audwl326@gmail.com>
2024-03-13 19:52:27 +0900

    6-24. GraphQL Query with Response Model Class

--- src/main/java/com/udemy/graphql_lecture_1/query/Query.java
+++ src/main/java/com/udemy/graphql_lecture_1/query/Query.java
@@ -5,9 +5,15 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import com.udemy.graphql_lecture_1.request.SampleRequest;
+import com.udemy.graphql_lecture_1.response.StudentResponse;
+import com.udemy.graphql_lecture_1.service.StudentService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class Query {
+    private final StudentService studentService;
 
     @QueryMapping
     public String firstQuery() {
@@ -27,5 +33,10 @@ public class Query {
     @QueryMapping
     public String fullName(@Argument SampleRequest sampleRequest) {
         return sampleRequest.getFirstName() + " " + sampleRequest.getLastName();
+    }
+
+    @QueryMapping
+    public StudentResponse student(long id) {
+        return new StudentResponse(studentService.getStudentById(id));
     }
 }
